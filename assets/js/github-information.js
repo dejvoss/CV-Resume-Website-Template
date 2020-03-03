c7e5389f71089ad728f025e41f6398bdbb29dfe1
deo <deosiecki@gmail.com>
2020-03-03 12:21:21 +0000

    fix the bugs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,4 +1,3 @@
-  
 function userInformationHTML(user) {
     return `
         <h2>${user.name}
@@ -20,23 +19,26 @@ function repoInformationHTML(repos) {
     if (repos.length == 0) {
         return `<div class="clearfix repo-list">No repos!</div>`;
     }
-    var listItemHtml = repos.map(function(repo) {
+
+    var listItemsHTML = repos.map(function(repo) {
         return `<li>
-                    <a href="${repo.html_url}" target="_blank">${repo.name}"</a>
-                    </li>`;
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
     });
-    
+
     return `<div class="clearfix repo-list">
-        <p>
-            <strong>Repo List:</strong>
-        </p>
-        <ul>
-            ${listItemsHtml.join("\n")}
-        </ul>
-        </div>`;
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -70,3 +72,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
